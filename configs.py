0343e789d05e777f5aa81f72616eb5c7a24b61bf
Selfie SD <webtamil0@gmail.com>
2022-02-02 10:39:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,25 +7,26 @@ class Config(object):
     API_ID = os.environ.get("API_ID")
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "VideoMergeDcBot")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
-    TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
+    TIME_GAP = int(os.environ.get("TIME_GAP", 4))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 10))
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
     MONGODB_URI = os.environ.get("MONGODB_URI")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1940030638))
 
     START_TEXT = """
-Hi Unkil, I am Video Merge Bot!
+Oii 🦊, I am Video Merge Bot!
+
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 
-Made by @AbirHasan2005
+Made by @GroupDcBots
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "Video Merged by @{}\n\nMade by @GroupDcBots"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
