3a293f8fecc6dedf591be0d6f4ce7b3624fd54be
Selfie SD <webtamil0@gmail.com>
2022-06-13 06:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, 
 QueueDB = {}
 ReplyDB = {}
 FormtDB = {}
-NubBot = Client
-    session_name=char(Config.SESSION_NAME),
+NubBot = Client(
+    session_name=Config.SESSION_NAME,
     api_id=int(Config.API_ID),
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
